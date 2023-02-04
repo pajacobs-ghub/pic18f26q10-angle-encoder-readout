82d8e2aa2933240ea2d043278985b8df0c8fef27
Peter Jacobs <peterj@mech.uq.edu.au>
2023-02-04 16:47:54 +1000

    Demo 2 gets UART1 working.
    To give some margin in the setting of the 115200 baud rate,
    increase the oscillator frequency to 32MHz.
    It was 16MHz in the original demo-1.

--- demo-2-uart.c
+++ demo-2-uart.c
@@ -1,7 +1,9 @@
-/* demo-1-flash-led.c
- * First code to bring up PIC18F26Q10-I/SP on encoder readout board.
- * PJ 2023-02-02
- */
+// demo-2-uart.c
+// Demonstrate transmitting and receiving characters 
+// with the PIC18F26Q10 EUSART1 peripheral.
+// PJ, 2019-04-15 adapted from pic16f18324 to PIC16F18426
+//     2023-02-03 adapted to PIC18F26Q10
+//
 // Configuration Bit Settings (generated from Config Memory View)
 // CONFIG1L
 #pragma config FEXTOSC = OFF
@@ -52,18 +54,38 @@
 
 #include <xc.h>
 #include "global_defs.h"
+#include "uart.h"
+#include <stdio.h>
+#include <string.h>
 
 #define GREENLED LATBbits.LATB5
 
-int main()
+
+int main(void)
 {
+    char buf[80];
+    char* buf_ptr;
+    int n;
     OSCFRQbits.HFFRQ = 0b0110; // Select 32MHz.
     TRISBbits.TRISB5 = 0; // Pin as output for LED.
     GREENLED = 0;
+    uart1_init(115200);
+    n = printf("\r\nMagnetic encoder readout board with PIC18F26Q10.");
     while (1) {
         GREENLED ^= 1;
-        __delay_ms(1000);
-        CLRWDT();
+        __delay_ms(500);
+        n = printf("\r\nEnter some text: ");
+        // Characters are echoed as they are typed.
+        // Backspace deleting is allowed.
+        buf_ptr = gets(buf);
+        n = printf("\rEntered text was: ");
+        if (buf_ptr) { puts(buf); }
+        if (strncmp(buf, "quit", 4) == 0) break;
+        n = printf("\r\nNow, do it again.");
     }
-    return 0; // We should never arrive here.
-}
+    uart1_flush_rx();
+    n = printf("Press any key to close the serial port.");
+    while (!kbhit()) { CLRWDT(); }
+    uart1_close();
+    return 0; // Expect that the MCU will reset.
+} // end main
